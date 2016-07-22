a8fe4ee2d7a8706ba107fecf3705e38a1ec87c82
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2016-07-21 23:51:58 -0300

    Add basic descriptive statistics calculation
    To be reviewed, confidence_interval is possibly wrong

--- tools/enhancement_test.py
+++ tools/enhancement_test.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 import re
-import sys
+import numpy
 import subprocess
+import scipy.stats as stats
 
 rcss_cmd = [
     'rcssserver', 'server::auto_mode=true', 'server::coach_w_referee=true',
@@ -11,7 +12,7 @@ rcss_cmd = [
 
 def run_match(team_a, team_b):
     cmd = [
-        *rcss_cmd, 'server::team_r_start={}'.format(team_a), 'server::team_l_start={}'.format(team_b)
+        *rcss_cmd, 'server::team_l_start={}'.format(team_a), 'server::team_r_start={}'.format(team_b)
     ]
     print('Running match "{}"'.format(' '.join(cmd)))
     output = subprocess.check_output(cmd, universal_newlines=True).splitlines()
@@ -22,16 +23,35 @@ def run_match(team_a, team_b):
             teams = (teams_re.group(1), teams_re.group(2))
         elif score_re:
             score = (score_re.group(1), score_re.group(2))
-    print('{}: {} - {}: {}', teams[0], score[0], teams[1], score[1])
+    print('{}: {} - {}: {}'.format(teams[0], score[0], teams[1], score[1]))
+    result = {teams[i]: score[i] for i in range(2)}
+    return result
+
+
+def parse_args():
+    import argparse
+    # TODO Improve this code and autodetect team names
+    parser = argparse.ArgumentParser(description='Run team enhancement test.')
+    parser.add_argument('team_a_program', help='Left team target program')
+    parser.add_argument('team_a_name', help='Left team name')
+    parser.add_argument('team_b_program', help='Right team target program')
+    parser.add_argument('team_b_name', help='Right team name')
+    parser.add_argument('-n', dest='match_count', type=int, default=10,
+                        help='Number of matches to run')
+    return parser.parse_args()
 
 
 def main():
-    # Use argparse in the future, add parameter for match count
-    # https://docs.python.org/3/library/argparse.html
-    if len(sys.argv) != 3:
-        print('Usage: {} left_team right_team'.format(sys.argv[0]))
-    for _ in range(10):
-        run_match(sys.argv[1], sys.argv[2])
+    args = parse_args()
+    results = [None] * args.match_count
+    for i in range(args.match_count):
+        scores = run_match(args.team_a_program, args.team_b_program)
+        results[i] = (scores[args.team_a_name], scores[args.team_b_name])
+    mean = numpy.mean(results, axis=0)
+    std_error = stats.sem(results, axis=0)
+    confidence_interval = [
+        stats.t.interval(0.95, len(mean) - 1, loc=mean[i], scale=std_error[i]) for i in range(2)
+    ]
 
 if __name__ == '__main__':
     main()
